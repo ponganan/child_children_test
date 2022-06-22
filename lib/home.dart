05e3da4110ec8eeabd40d645e4425143dd8a3912
Admin <joeyubu@gmail.com>
2022-06-22 14:47:51 +0700

    make column and container and set alignment

--- lib/home.dart
+++ lib/home.dart
@@ -14,13 +14,78 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Child and Children Test'),
+        title: Text(
+          'Child and Children Test',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.purple,
+          ),
+        ),
         centerTitle: true,
         backgroundColor: Colors.green,
       ),
-      body: Container(
-        height: 450,
-        color: Colors.pink,
+      body: Center(
+        child: Container(
+          alignment: Alignment.center,
+          height: 450,
+          width: 250,
+          color: Colors.pink,
+
+          // child: Text(
+          //   'JOey',
+          //   style: TextStyle(
+          //       fontSize: 55,
+          //       fontWeight: FontWeight.bold,
+          //       color: Colors.amberAccent),
+          // ),
+
+          child: Column(
+            //make this column to center
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 60,
+                width: 100,
+                color: Colors.purple,
+                //make this text to center of container
+                alignment: Alignment.center,
+                child: Text(
+                  'Text 1',
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Container(
+                height: 60,
+                width: 100,
+                color: Colors.green,
+                alignment: Alignment.center,
+                child: Text(
+                  'Text 2',
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Container(
+                height: 60,
+                width: 100,
+                color: Colors.yellow,
+                alignment: Alignment.center,
+                child: Text(
+                  'Text 3',
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
